19e9fc13e1cabf588b33dd15c2cdebd1791e0a7c
justinbiebergoth <hiirari666@gmail.com>
2022-12-13 14:24:00 +0300

    Initial Commit

---
+++ ColorSwitcherSui/ColorSwitcherSuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSwitcherSuiApp.swift
+//  ColorSwitcherSui
+//
+//  Created by hiirari on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSwitcherSuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
